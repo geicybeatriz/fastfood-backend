5896d623ecdd3e84669b145ee9f31c9b4477381d
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2023-11-25 00:13:11 -0300

    feat: :sparkles: create file with additionals data

---
+++ prisma/factory/additionalsFactory.ts
@@ -0,0 +1,18 @@
+const additionals = [
+  {
+    name: 'Bacon',
+    description: 'Bacon extra',
+    picture:
+      'https://img.freepik.com/fotos-premium/fotografia-de-produto-fotorrealista-fotografia-de-alimentos-baco_670382-60443.jpg',
+    price: 2.0,
+  },
+  {
+    name: 'Queijo',
+    description: 'Queijo extra',
+    picture:
+      'https://www.mundoboaforma.com.br/wp-content/uploads/2015/05/download-33.jpg',
+    price: 2.0,
+  },
+];
+
+export default additionals;
