0246dd62c79d6dca1bb69a1c5fa03ef1908b3fd8
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2023-11-25 15:02:00 -0300

    feat: :heavy_plus_sign: add test configurations to the project

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
